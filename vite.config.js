a3d0f4222732d6723f5839c1e0fd9547eb640186
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-02-22 15:12:02 +0200

    Prepare deployable GitHub page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/IBM-ReactJS-Final-Project-Paradise-Nursery-Shopping-Application",
   plugins: [react()],
-})
+});
