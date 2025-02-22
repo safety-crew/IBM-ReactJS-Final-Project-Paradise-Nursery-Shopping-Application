f0a2d1e8a99520b635b1d25b3a3baf3a669c3c9a
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-02-22 16:35:58 +0200

    Finalize Project Files and Fix Bugs

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,34 +1,37 @@
-/* eslint-disable react-refresh/only-export-components */
 import { createSlice } from "@reduxjs/toolkit";
 
-export const CartSlice = createSlice({
+export const CreatSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-      const { name, image, cost } = action.payload;
-      const existingItem = state.items.find((item) => item.name === name);
+      const existingItem = state.items.find(
+        (item) => item.name === action.payload.name
+      );
       if (existingItem) {
-        existingItem.quantity++;
+        existingItem.quantity += 1;
       } else {
-        state.items.push({ name, image, cost, quantity: 1 });
+        state.items.push({ ...action.payload, quantity: 1 });
       }
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter((item) => item.name !== action.payload);
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
     },
     updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find((item) => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+      if (item) {
+        item.quantity = action.payload.quantity;
       }
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
 
-export default CartSlice.reducer;
+export default CreatSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./CartSlice";
+import cartReducer from "./CreatSlice";
+
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
 });
+
 export default store;
